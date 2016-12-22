6f45b688c83c2a8008289f36e1e06a3b3278926a
abaker992 <abaker992@gmail.com>
2016-12-22 00:06:09 -0500

    Add files via upload

---
+++ lessons/src/lessons/Lessons.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lessons;
+
+/**
+ *
+ * @author Baker
+ */
+//getting user input
+import java.util.Scanner;
+public class Lessons {
+    //this is method header/method
+    //java,much like C's need to use a main function else it wont work
+    public static void main(String args[]){
+        //printing a statement
+        System.out.println("Hello Youtube");
+         
+        //assigning variables 
+        double tuna;
+        tuna = 5.38;
+        System.out.println(tuna+" "+"pounds");
+        //print ln makes the NEXT print statement 
+        // go to the new line
+        System.out.print("like that");
+        System. out.println("next lesson");
+        
+        //getting user input
+        //system in = system input
+        Scanner Adam = new Scanner(System.in);
+        //print  output
+        System.out.println(Adam.nextLine());
+    
+    }
+    
+    }
+    
+
